275008b4638d32cda2bca4f96acc9cac8392a792
Lisa <89645136+LisaAlaz@users.noreply.github.com>
2022-03-31 11:49:13 +0100

    rolled back config

--- view/src/config.js
+++ view/src/config.js
@@ -9,7 +9,6 @@ import FeedbackOptions from "./widgets/options/GeneralOptions/FeedbackOptions";
 import EmotionOptions from "./widgets/options/GeneralOptions/EmotionOptions";
 import EventOptions from "./widgets/options/GeneralOptions/EventOptions";
 import YesNoProtocolOptions from "./widgets/options/GeneralOptions/YesNoProtocolsOptions";
-import Robert from "./Robert.png"
 const botName = "SATbot";
 
 const config = {
@@ -32,10 +31,7 @@ const config = {
   customComponents: {
     header: () => <div style={{height: '16px', fontFamily: 'Arial', borderTopLeftRadius: '5px', borderTopRightRadius: '5px',
     background: '#EBECED', color: '#4A4A4A', padding: '8px', borderBottom: '1px solid #B8BABA'}}>SATbot</div>,
-    botAvatar: () => <div class="react-chatbot-kit-chat-bot-avatar-container" style={{fontFamily: 'Arial', fontSize: '10px'}}><img src={Robert}/></div>,
-    robertAvatar: () => <div class="react-chatbot-kit-chat-bot-avatar-container" style={{fontFamily: 'Arial'}}>
-                           <img src={Robert}/>
-                        </div>
+    botAvatar: () => <div class="react-chatbot-kit-chat-bot-avatar-container" style={{fontFamily: 'Arial'}}><p class="react-chatbot-kit-chat-bot-avatar-letter">S</p></div>
   },
 
   widgets: [
