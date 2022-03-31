e9775d36e1f051b5412bec9218e33cee9398f57a
Lisa <89645136+LisaAlaz@users.noreply.github.com>
2022-03-31 01:25:13 +0100

    reduced sample for faster response time

--- model/rule_based_model.py
+++ model/rule_based_model.py
@@ -624,7 +624,7 @@ class ModelDecisionMaker:
         #return random.choice(column.dropna().sample(n=15).to_list()) #using random choice instead of machine learning
         maxscore = 0
         chosen = ''
-        for row in column.dropna().sample(n=15): #was 25
+        for row in column.dropna().sample(n=5): #was 25
              fitscore = get_sentence_score(row, prev_qs)
              if fitscore > maxscore:
                  maxscore = fitscore
@@ -632,7 +632,7 @@ class ModelDecisionMaker:
         if chosen != '':
             return chosen
         else:
-            return random.choice(column.dropna().sample(n=15).to_list()) #was 25
+            return random.choice(column.dropna().sample(n=5).to_list()) #was 25
 
 
     def split_sentence(self, sentence):
