66dea57e7c551fb4446191b28f3e33f1473ec569
zeena <zeenapatel2@gmail.com>
2022-06-02 02:21:59 +0100

    Initialise initial user state to positive

--- model/__init__.py
+++ model/__init__.py
@@ -127,9 +127,12 @@ def create_app():
             decision_maker.pre_compute_empathy_scores()
             decision_maker.pre_compute_fluency_scores()
             decision_maker.pre_compute_humour_scores()
+            decision_maker.initialise_user_states_initial(user.id)
             decision_maker.initialise_remaining_choices(user.id)
             decision_maker.initialise_prev_questions(user.id)
             decision_maker.initialise_user_session_vars(user.id)
+            # TODO testing the retrieval function
+            decision_maker.test_retrieval_function()
             decision_maker.clear_suggestions(user.id)
             decision_maker.clear_choices(user.id)
             decision_maker.clear_persona(user.id)
